c0deae3c02b89512d3e9a6dba02a2af2fc9fb5ce
Kiran Basnet <kb916980@gmail.com>
2026-07-20 20:07:42 +0100

    Fix signup auth and deployment config

--- hamro-shikshya-backend/models/School.js
+++ hamro-shikshya-backend/models/School.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-const cleanText = (value) => String(value || "").trim();
-
 const addressDetailsSchema = new mongoose.Schema(
   {
     formattedAddress: {
@@ -71,7 +69,6 @@ const schoolSchema = new mongoose.Schema(
       type: String,
       required: [true, "School name is required."],
       trim: true,
-      alias: "name",
     },
 
     address: {
@@ -140,66 +137,20 @@ const schoolSchema = new mongoose.Schema(
       default: true,
     },
   },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-      transform: (doc, ret) => {
-        ret.id = ret._id;
-        ret.name = ret.schoolName;
-        delete ret.__v;
-        return ret;
-      },
-    },
-    toObject: {
-      virtuals: true,
-    },
-  }
+  { timestamps: true }
 );
-
-schoolSchema.pre("validate", function (next) {
-  const possibleSchoolName = cleanText(
-    this.schoolName ||
-      this.name ||
-      this.get("name") ||
-      this._doc?.name ||
-      this._doc?.school ||
-      this._doc?.school_name
-  );
-
-  if (possibleSchoolName) {
-    this.schoolName = possibleSchoolName;
-  }
-
-  this.email = cleanText(this.email).toLowerCase();
-  this.address = cleanText(this.address);
-  this.phone = cleanText(this.phone);
-  this.website = cleanText(this.website);
-  this.principalName = cleanText(this.principalName);
-  this.adminName = cleanText(this.adminName);
-  this.logoUrl = cleanText(this.logoUrl);
-
-  const latitude = this.addressDetails?.latitude;
-  const longitude = this.addressDetails?.longitude;
-
-  if (
-    typeof latitude === "number" &&
-    typeof longitude === "number" &&
-    !Number.isNaN(latitude) &&
-    !Number.isNaN(longitude)
-  ) {
-    this.location = {
-      type: "Point",
-      coordinates: [longitude, latitude],
-    };
-  }
-
-  next();
-});
 
 schoolSchema.index({ location: "2dsphere" });
 schoolSchema.index({ schoolName: 1 });
 schoolSchema.index({ email: 1 });
+
+schoolSchema.set("toJSON", {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret.__v;
+    return ret;
+  },
+});
 
 const School = mongoose.models.School || mongoose.model("School", schoolSchema);
 
